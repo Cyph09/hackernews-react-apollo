982fd32a6c659badd62c4f5a224264a86c5cc57b
Swaleh Mhina <mhinasw@gmail.com>
2020-10-19 11:55:30 +0300

    Deploy the prisma db service

--- server/src/generated/prisma-client/index.js
+++ server/src/generated/prisma-client/index.js
@@ -20,21 +20,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: ``,
-  secret: `mysecret123`
+  endpoint: `https://eu1.prisma.sh/swalehe-mhina-e9ec84/react-apollo/react-apollo-db`
 });
 exports.prisma = new exports.Prisma();
-var models = [
-  {
-    name: "Link",
-    embedded: false
-  },
-  {
-    name: "User",
-    embedded: false
-  },
-  {
-    name: "Vote",
-    embedded: false
-  }
-];
