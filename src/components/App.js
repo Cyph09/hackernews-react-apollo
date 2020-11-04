5f26b4d78a076d43c023f5ca2dc371da9e0ba737
Swaleh Mhina <mhinasw@gmail.com>
2020-11-04 15:43:17 +0300

    Fix typo

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,21 @@ import {Switch, Route} from 'react-router-dom'
 import Header from './Header'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
-import Login from "./Login";
+import Login from './Login';
 
 class App extends Component{
-  render(){
+  render() {
     return (
-    <div className="center w85">
-      <Header />
-      <div className="ph3 pv1 background-gray">
-        <Switch>
-          <Route exact path="/" component={LinkList} />
-          <Route exact paht="/create" component={CreateLink} />
-          <Route exact path="/login" component={Login}/>
-        </Switch>
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
       </div>
-    </div>
     )
   }
 }
