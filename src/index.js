e5f6a743676bf94e0582de0ee790dc04c47e5e98
Swaleh Mhina <mhinasw@gmail.com>
2020-11-06 11:58:26 +0300

    Authenticating request via Apollo

--- src/index.js
+++ src/index.js
@@ -7,14 +7,26 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-boost'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
 import { BrowserRouter } from 'react-router-dom';
+import { AUTH_TOKEN } 
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
